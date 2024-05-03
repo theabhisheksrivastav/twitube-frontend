9c194bc105e2ab7b6251813414b69bc5c44bd8ab
abhisheksrivastavofficial <abhisheksrivastavofficial@gmail.com>
2024-05-03 11:21:38 +0530

    Update App.jsx to include Ant Design icons and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,84 @@
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      <h1 className="bg-slate-600 w-full flex justify-center min-h-96">hello twitube</h1>
-    </>
-  )
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
-
-export default App
+const items = [
+  getItem('Profile', '1', <PieChartOutlined />),
+  getItem('Twits', '2', <DesktopOutlined />),
+  getItem('Videos', '3', <UserOutlined />),
+  getItem('Comments', '4', <TeamOutlined />),
+  getItem('Likes', '5', <FileOutlined />),
+];
+const App = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          TwiTube Header ICON
+        </Header>
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
